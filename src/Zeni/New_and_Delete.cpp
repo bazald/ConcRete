ae7840e9f9934768ff56f977f698ac4bc48587bb
Mitchell Keith Bloch <bazald@gmail.com>
2018-07-02 12:50:44 -0400

    Add jemalloc for Windows x64;

---
+++ src/Zeni/New_and_Delete.cpp
@@ -0,0 +1,23 @@
+#include <memory>
+
+#include "jemalloc/jemalloc.h"
+
+#if defined(_MSC_VER) && defined(_M_AMD64)
+
+void * operator new(size_t size) noexcept(false) {
+  return je_malloc(size);
+}
+
+void * operator new(size_t size, std::align_val_t alignment) noexcept(false) {
+  return je_aligned_alloc(size_t(alignment), size);
+}
+
+void operator delete(void * ptr) noexcept {
+  je_free(ptr);
+}
+
+void operator delete(void * ptr, std::align_val_t) noexcept {
+  je_free(ptr);
+}
+
+#endif
